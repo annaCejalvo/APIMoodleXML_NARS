92873d616b08deff632a837452a23fad0524a128
annaCejalvo <anna.cejalvo@gmail.com>
2012-03-02 12:19:28 +0100

    first commit

---
+++ APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/App.java
@@ -0,0 +1,13 @@
+package dcll.nars.apimoodlexml_nars;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
