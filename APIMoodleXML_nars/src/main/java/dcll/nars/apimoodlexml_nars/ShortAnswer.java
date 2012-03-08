13816a60ec1bf842e5b01b54804ec3c8f8deb1a7
annaCejalvo <anna.cejalvo@gmail.com>
2012-03-08 09:28:33 +0100

    Ajout de la classe ShortAnswer Ajout de la librairie JDOM

---
+++ APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/ShortAnswer.java
@@ -0,0 +1,18 @@
+package dcll.nars.apimoodlexml_nars;
+
+import java.io.*;
+import org.jdom.*;
+import org.jdom.output.*;
+
+public class ShortAnswer {
+
+	static Element racine = new Element("question");
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
