b058de066bb086148d37f649975b5ce20dbca83d
annaCejalvo <anna.cejalvo@gmail.com>
2012-03-12 09:44:04 +0100

    modification du pom (ajout de la dépendance jdom)

--- APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/ShortAnswer.java
+++ APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/ShortAnswer.java
@@ -4,7 +4,7 @@ import java.io.*;
 import org.jdom.*;
 import org.jdom.output.*;
 
-public class ShortAnswer extends Question{
+public class ShortAnswer /*extends Question*/{
 
 	static Element racine = new Element("question");
 	
@@ -14,10 +14,41 @@ public class ShortAnswer extends Question{
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		Attribute questionA = new Attribute("type", "shortanswer");
+		
 		Element answer = new Element("answer");
 		racine.addContent(answer);
 		
-
+		Element text = new Element("text");
+		text.setText("The correct answer");
+		answer.addContent(text);
+		Element feedback = new Element("feedback");
+		answer.addContent(feedback);
+		Element textFeedBack = new Element("text");
+		textFeedBack.setText("Correct");
+		feedback.addContent(textFeedBack);
+		
+		affiche();
+		enregistre("ShortAnswer.xml");
 	}
 
+	static void affiche(){
+		try{
+		//On utilise ici un affichage classique avec getPrettyFormat()
+		XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+		sortie.output(document, System.out);
+		}
+		catch (java.io.IOException e){}
+	}
+
+	static void enregistre(String fichier){
+		try{
+		//On utilise ici un affichage classique avec getPrettyFormat()
+		XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+		//Remarquez qu'il suffit simplement de créer une instance de FileOutputStream
+		//avec en argument le nom du fichier pour effectuer la sérialisation.
+		sortie.output(document, new FileOutputStream(fichier));
+		}
+		catch (java.io.IOException e){}
+	}
 }
