9b273aa12fdbc1c35cff6b82627338f21df77169
annaCejalvo <anna.cejalvo@gmail.com>
2012-03-12 08:28:06 +0100

    ajout de la classe Question

---
+++ APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/Question.java
@@ -0,0 +1,13 @@
+package dcll.nars.apimoodlexml_nars;
+
+import java.util.List;
+
+public class Question {
+	private List<String> reponses;
+	private String intitule;
+	
+	/*public Question(String intitule){
+		this.intitule = intitule;
+		//this.reponses = new List();
+	}*/
+}
