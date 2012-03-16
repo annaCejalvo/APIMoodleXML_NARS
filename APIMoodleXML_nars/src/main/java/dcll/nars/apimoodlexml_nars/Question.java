bca11cf258ddd9ae20edb65e9c38c7f4fb049bc7
annaCejalvo <anna.cejalvo@gmail.com>
2012-03-16 16:43:52 +0100

    modification de la classe Question

--- APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/Question.java
+++ APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/Question.java
@@ -3,35 +3,25 @@ package dcll.nars.apimoodlexml_nars;
 import java.io.FileOutputStream;
 import java.util.List;
 
+import org.jdom.Attribute;
+import org.jdom.Document;
+import org.jdom.Element;
 import org.jdom.output.Format;
 import org.jdom.output.XMLOutputter;
 
 public class Question {
-	private List<String> reponses;
+	//private List<String> reponses;
 	private String intitule;
+	private String type;
+
+	static Element question = new Element("question");
 	
-	/*public Question(String intitule){
+	public Question(String intitule, String type){
 		this.intitule = intitule;
-		//this.reponses = new List();
-	}*/
-	
-	static void affiche(org.jdom.Document document){
-		try{
-		//On utilise ici un affichage classique avec getPrettyFormat()
-		XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
-		sortie.output(document, System.out);
-		}
-		catch (java.io.IOException e){}
+		this.type = type;
+		
+		Attribute questionAttribute = new Attribute("type", this.type);
+		question.setAttribute(questionAttribute);
 	}
 
-	static void enregistre(org.jdom.Document document, String fichier){
-		try{
-		//On utilise ici un affichage classique avec getPrettyFormat()
-		XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
-		//Remarquez qu'il suffit simplement de créer une instance de FileOutputStream
-		//avec en argument le nom du fichier pour effectuer la sérialisation.
-		sortie.output(document, new FileOutputStream(fichier));
-		}
-		catch (java.io.IOException e){}
-	}
 }
