0e57052d328ab42a3566799360304c75774fdf89
NADA <haddaji.nada@gmail.com>
2012-03-16 17:33:27 +0100

    classe reponse

---
+++ APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/Reponse.java
@@ -0,0 +1,13 @@
+package dcll.nars.apimoodlexml_nars;
+
+public class Reponse {
+	String textReponse;
+	String feedBack;
+	float fraction;
+	public Reponse(String reponse, String feedBack, float fraction) {
+		this.textReponse = reponse;
+		this.feedBack = feedBack;
+		this.fraction = fraction;
+	}
+	
+}
