c6efe8dd98ef292f75cd53d2f6f8ecc8c883c0e8
NADA <haddaji.nada@gmail.com>
2012-03-20 22:14:49 +0100

    #7 "convertXMLToJava" finished

--- APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/read_questions/ReadQuiz.java
+++ APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/read_questions/ReadQuiz.java
@@ -31,7 +31,8 @@ public class ReadQuiz {
 		// On cr�e une List contenant tous les noeuds "question" de l'Element
 		// racine
 		List listQuestions = racine.getChildren("question");
-
+		System.out.println("Root element of the doc is "+ racine.getName());
+		System.out.println("Total of questions "+ racine.getChildren().size());
 		// On cr�e un Iterator sur notre liste
 		Iterator i = listQuestions.iterator();
 		while (i.hasNext()) {
@@ -39,12 +40,18 @@ public class ReadQuiz {
 			// pouvoir utiliser les m�thodes propres aux Element comme :
 			// selectionner un noeud fils, modifier du texte, etc...
 			Element question = (Element) i.next();
+			System.out.println("\tquestion type=\""+question.getAttributeValue("type")+"\"");
+			System.out.println("\t\tnom question : "+ question.getChild("nom").getChild("text").getText());
+			System.out.println("\t\tquestion format=\""+ question.getChild("questiontext").getAttributeValue("format")+"\"");
+			System.out.println("\t\t\ttext question : "+ question.getChild("questiontext").getChild("text").getText());
 			// On affiche le nom de l'element courant
 			List listReponses = question.getChildren("reponse");
 			Iterator j = listReponses.iterator();
 			while (j.hasNext()) {
 				Element reponse = (Element) j.next();
-				System.out.println(reponse.getChild("text").getText());
+				System.out.println("\t\treponse fraction=\""+reponse.getAttributeValue("fraction")+"\"");
+				System.out.println("\t\t\ttext reponse : "+reponse.getChild("text").getText());
+				System.out.println("\t\t\tfeedback reponse : "+reponse.getChild("feedback").getChild("text").getText());
 			}
 			//System.out.println(courant.getChild("questiontext").getText());
 			//System.out.println(courant.getChild("reponse").getText());
