83ad49630e471e17f2d1fe9ba2bab6a5bf76af6a
NADA <haddaji.nada@gmail.com>
2012-03-20 22:24:52 +0100

    suppression des fichiers qui servent plus à rien

--- APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/write_questions/WriteTrueFalseQuestion.java
+++
@@ -1,9 +0,0 @@
-package dcll.nars.apimoodlexml_nars.write_questions;
-
-public class WriteTrueFalseQuestion extends Question {
-
-	public WriteTrueFalseQuestion(String nomQ, String textQ, String type) {
-		
-		super(nomQ, textQ, type);
-	}	
-}
