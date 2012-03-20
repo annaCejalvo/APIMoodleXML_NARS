dfe72f986ccadc3d61c5d0ac3fe952fce3a21bd6
mosrati <ramzi.mosrati@gmail.com>
2012-03-20 11:26:54 +0100

    modification class Question

--- APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/Numerical.java
+++ APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/Numerical.java
@@ -1,22 +1,42 @@
 package dcll.nars.apimoodlexml_nars;
 
+import java.io.FileOutputStream;
+
 import org.jdom.Attribute;
 import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.output.Format;
+import org.jdom.output.XMLOutputter;
 
 public class Numerical extends Question  {
 	
-     static Element question = new Element("question");
+	
+	static Element question = new Element("question");
 	
 	static org.jdom.Document document = new Document(question);
+	
+     public Numerical(String nomQ, String textQ, String type) {
+		super(nomQ, textQ, type);
+		// TODO Auto-generated constructor stub
+		
+	
+		
+	}
+
+
+	//static Element question = new Element("question");
+	
+	//static org.jdom.Document document = new Document(question);
 	
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		Attribute questionA = new Attribute("type", "Numerical ");
-		question.setAttribute(questionA);
+     public void addContents(){
+    	 
+		//Attribute questionA = new Attribute("type", "Numerical ");
+		//question.setAttribute(questionA);
+    	 
 		
 		Element answer = new Element("answer");
 		Attribute answerA = new Attribute ("fraction", "100");
@@ -34,10 +54,44 @@ public class Numerical extends Question  {
 		textFeedBack.setText("Feedback");
 		feedback.addContent(textFeedBack);
 		
-		affiche(document);
-		enregistre(document, "Numerical.xml");
+	
+		
+     }		
+		
+		 public  void affich() {
+
+			    try{
+
+			    //On utilise ici un affichage classique avec getPrettyFormat()
+
+			    XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());
+
+			    sortie.output(document, System.out);
+
+			   }
+
+			    catch (java.io.IOException e){}
+
+			  }  	
+
+			   static void enregistre(String fichier){
+			  	 
+			    try{
+
+             //On utilise ici un affichage classique avec getPrettyFormat()	
+
+			    XMLOutputter sortie = new XMLOutputter(Format.getPrettyFormat());  	
+
+			    sortie.output(document, new FileOutputStream(fichier));
+			  }
+			catch (java.io.IOException e){}
+			   }
+
+				//affich(document);
+				//enregistre(document, "Numerical.xml");
+				
 		
 		
-	}
+	
 
 }
--- APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/ShortAnswer.java
+++ APIMoodleXML_nars/src/main/java/dcll/nars/apimoodlexml_nars/ShortAnswer.java
@@ -1,4 +1,4 @@
-=package dcll.nars.apimoodlexml_nars;
+package dcll.nars.apimoodlexml_nars;
 
 import org.jdom.*;
 
